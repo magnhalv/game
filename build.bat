dfcaadd50c890ec44947b7af3cc43a75359f68bb
Magnus Halvorsen <magnus.halvorsen@acando.no>
2021-06-02 09:34:42 +0200

    Optimize compiler flags

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 IF NOT EXIST build mkdir build
 pushd build
-cl -WX -W4 -wd4201 -wd4100 -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Zi  ../src/main.cpp user32.lib Gdi32.lib
+cl -MT -nologo -GR- -Gm- -Oi -EHa- -WX -W4 -wd4201 -wd4100 -Fmwin32_game.map -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Zi  ../src/main.cpp user32.lib Gdi32.lib
 popd
