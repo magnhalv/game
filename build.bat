2f9e98de7df9273de1eb29a36ed202fe2e1c933f
Magnus Halvorsen <magnus.halvorsen@acando.no>
2021-06-08 21:24:01 +0200

    Move compiler flags into separate variable

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 
+set CommonCompilerFlags=-MT -nologo -GR- -Gm- -Oi -EHa- -WX -W4 -wd4201 -wd4100 -Fmwin32_game.map -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Zi
+
 IF NOT EXIST build mkdir build
 pushd build
-cl -MT -nologo -GR- -Gm- -Oi -EHa- -WX -W4 -wd4201 -wd4100 -Fmwin32_game.map -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Zi  ../src/main.cpp user32.lib Gdi32.lib
+cl %CommonCompilerFlags%  ../src/main.cpp user32.lib Gdi32.lib
 popd
