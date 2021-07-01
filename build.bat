f3b2689dbc171edf2d53e5234e3d2247e6fb5245
Magnus Halvorsen <magnus.halvorsen@acando.no>
2021-07-01 15:35:33 +0200

    Disable unused function/variables compiler warning

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Z7 /std:c++latest
+set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4505 -wd4100 -wd4189 -DGAME_INTERNAL=1 -DGAME_SLOW=1 -FC -Z7 /std:c++latest
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib Gdi32.lib winmm.lib
 
 set CurrentDateTime=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
